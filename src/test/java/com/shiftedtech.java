32004554b4b4057e0d9dd7d10948e2936940d753
ashrafulkhan <islam49968@yahoo.com>
2018-04-08 19:19:56 -0400

    third commit of git

--- src/test/java/com/shiftedtech.java
+++ src/test/java/com/shiftedtech.java
@@ -3,5 +3,11 @@ package com;
 public class shiftedtech {
     public void test(){
         System.out.println("test case is procedure ");
+
+
+    }
+
+    public void test01(){
+        System.out.println("second test for git ");
     }
 }
